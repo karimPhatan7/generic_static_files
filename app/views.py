7b0099cd20f577212a3bc75ebae2444a9454b2e8
karimPhatan7 <mrd313888@gmail.com>
2023-11-22 20:27:16 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def karim(request):
+    return render(request,'karim.html') 
+
+def developer(request):
+    return render(request,'developer.html')
